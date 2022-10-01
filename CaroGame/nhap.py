897568437c8ba61c9099dd4fe4c2609dcefbb0d6
BIMSangDQ <bimsangdq@gmail.com>
2022-10-01 12:33:51 +0700

    finish

--- CaroGame/nhap.py
+++ CaroGame/nhap.py
@@ -1,6 +1,7 @@
 from ast import Pass
 from concurrent.futures import thread
 from tkinter import *
+from tkinter import messagebox
 from PIL import ImageTk,Image  
 import time
 import threading
@@ -28,47 +29,145 @@ Xturn.set('Đến lượt')
 Oturn = StringVar()
 Oturn.set('Đến lượt')
 
-def btn_enter(event):
-    event.widget['bg']= 'lightblue'
-    
-def btn_leave(event):
-    event.widget['bg']='white'
+positionX = []
+positionO = []
+cell=[]
 
 def btn_click(event):
     if count.get()==1:
-        threading.Thread(target=restart,name="1").start()
+        threading.Thread(target=startcountdown,name="1").start()
+
+    position = GetPositionWidget(event.widget.grid_info())
+    global positionX
+    global positionO
 
     if event.widget['text'] == '':
         if count.get()%2==0:
             event.widget['text']='O'
             event.widget['font'] = ('Arial',8,'bold')
             event.widget['fg']='red'
+            positionO.append(position)
             Xturn.set('')
             Oturn.set('Đến lượt')
         else:
             event.widget['text']='X'
             event.widget['font'] = ('Arial',8,'bold')
             event.widget['fg']='blue'
+            positionX.append(position)
             Oturn.set('')
             Xturn.set('Đến lượt')
         count.set(count.get()+1)
         countdown.set(30)
 
-def setX():
-    Pass
+        if len(positionO)>0:
+            if checkWin(positionO):
+                SetScore('O')
+        if len(positionX)>0:       
+            if checkWin(positionX):
+                SetScore('X')
 
-def setY():
-    Pass
+def SetScore(team):
+    MsgBox = messagebox.askquestion ('CaroGame',f'Team {team} thắng!\n Các bạn có muốn tiếp tục ván mới không?')
+    if team == 'O':
+        countOwin.set(countOwin.get()+1)
+    else:
+        countXwin.set(countXwin.get()+1)
 
-def checkWin():
-    Pass
+    if MsgBox == 'yes':
+        NewGame()      
+    else:
+        root.destroy()
 
-def restart():
+def NewGame():
+    global cell
+    global positionX
+    global positionO
+    for btn in cell:
+        btn['text'] = ''
+    positionX = []
+    positionO = []
+    countdown.set(30)
+
+def GetPositionWidget(grid_info):
+    infos = str(grid_info).split(', ')
+    column = 0
+    row = 0
+    for info in infos:
+        if info.count("'column': ")>0:
+            column = int(info.replace("'column':" , ""))
+        elif info.count("'row': ")>0:
+            row = int(info.replace("'row':" , ""))
+    position=[column,row]
+    return position
+
+def checkWin(arr):
+    #Check hàng ngang
+    for i in range(0,len(arr),1):
+        pos = arr[i]
+        result = True
+        for j in range(1,5,1):
+            if findPosition(arr,[pos[0]+j,pos[1]]) == False:
+                result = False
+                break
+        if result:
+            return True
+
+    #Check hàng dọc
+    for i in range(0,len(arr),1):
+        pos = arr[i]
+        result = True
+        for j in range(1,5,1):
+            if findPosition(arr,[pos[0],pos[1]+j]) == False:
+                result = False
+                break
+        if result:
+            return True
+
+    #Check xiên 1
+    for i in range(0,len(arr),1):
+        pos = arr[i]
+        result = True
+        for j in range(1,5,1):
+            if findPosition(arr,[pos[0]+j,pos[1]+j]) == False:
+                result = False
+                break
+        if result:
+            return True
+
+    #Check xiên 2
+    for i in range(0,len(arr),1):
+        pos = arr[i]
+        result = True
+        for j in range(1,5,1):
+            if findPosition(arr,[pos[0]+j,pos[1]-j]) == False:
+                result = False
+                break
+        if result:
+            return True
+    return False
+
+def findPosition(arrp,pt):
+    """
+    arrp: array vị trí các quân cờ
+    pt: vị trí cần tìm
+
+    return: True nếu tìm thấy và False nếu không tìm thấy
+    """
+    for p in arrp:
+        if p[0] == pt[0] and p[1] == pt[1]:
+            return True
+    return False
+
+def startcountdown():
     countdown.set(30)
     while True:
         for i in range(0,30,1):
-            countdown.set(countdown.get()-1)
-            time.sleep(1)
+            try:
+                countdown.set(countdown.get()-1)
+                time.sleep(1)
+            except:
+                print("End thread")
+                return
         count.set(count.get()+1)
         if count.get()%2==0:
             Xturn.set('')
@@ -77,7 +176,6 @@ def restart():
             Oturn.set('')
             Xturn.set('Đến lượt')
         countdown.set(30)
-
 
 ##
 frameInfo = LabelFrame( root, relief = FLAT,bg='white', bd=10)
@@ -121,13 +219,14 @@ canvasO = Canvas(frame_banco, width = 100,bg='white',highlightbackground = 'whit
 canvasO.grid(row=2,rowspan=19,column=0,sticky=NSEW)
 canvasO.create_image(10,10, anchor=NW, image=photo_O)
 
+z=0
 for i in range(0,20,1):
     for j in range(1,21,1):
-        cell = Button(frame_banco,bg='white',width=2)#,command=lambda:threading.Thread(target=restart,name=count.get()).start())
-        cell.grid(row=i,column=j,sticky=NSEW)
-        cell.bind('<Button-1>',btn_click)
-        cell.bind('<Enter>', btn_enter)
-        cell.bind('<Leave>', btn_leave)
+        btncell = Button(frame_banco,bg='white',width=2)
+        btncell.grid(row=i,column=j,sticky=NSEW)
+        btncell.bind('<Button-1>',btn_click)
+        cell.append(btncell)
+        z+=1
 
 loadX = Image.open('CaroGame\images\X.jpg')
 resized_imageX= loadX.resize((100,100))
@@ -138,7 +237,5 @@ canvasX.create_image(10,10, anchor=NW, image=photo_X)
 
 lb_Oturn = Label(frame_banco,bg="white",fg="Green",textvariable=Oturn,font=('Arial',14))
 lb_Oturn.grid(row=0,rowspan=2,column=21,sticky=NSEW)
-
-
 
 root.mainloop()
--- cal.py
+++ cal.py
@@ -1,5 +1,3 @@
-from ast import Try
-from posixpath import split
 from tkinter import *
 
 highlightcolor = 'lightblue'
@@ -26,6 +24,7 @@ window.rowconfigure(5,weight = 2)
 window.rowconfigure(6,weight = 2)
 window.configure(bg=highlightcolor)
 
+
 #event
 def btn_enter(event):
     event.widget['bg']= highlightcolor
@@ -33,62 +32,6 @@ def btn_enter(event):
 def btn_leave(event):
     event.widget['bg']='SystemButtonFace'
 
-def btn_click(event):
-    if et_result['text'] == '0' or et_result['text'].count('=')==1:
-        et_result['text'] =  event.widget['text']
-    else:
-        et_result['text'] += event.widget['text']
-
-def btn_equalclick(event):
-    result = 0
-    inputtext = et_result['text']
-    inputtext = inputtext.replace('-','+-')
-    cals = inputtext.split('+')
-    for subtext in cals:
-        print(subtext)
-        if subtext.count('*')==0 and subtext.count('/')==0:
-            try:
-                result += float(subtext)
-            except:
-                result += 0
-        elif subtext.count('/')==0:
-            numbers = subtext.split('*')
-            subresult = float(numbers[0])
-            for i in range(1,len(numbers)):
-                subresult *= float(numbers[i])
-            result += subresult
-        elif subtext.count('*')==0:
-            numbers = subtext.split('/')
-            subresult = float(numbers[0])
-            for i in range(1,len(numbers)):
-                subresult /= float(numbers[i])
-            result += subresult
-        else:
-            numbers = subtext.split('*')
-            subresult = 1
-            for number in numbers:
-                if number.count('/')==0:
-                    subresult = subresult*float(number)
-                else:
-                    subnum = number.split('/')
-                    sub = float(subnum[0])
-                    for i in range(1,len(subnum)):
-                        sub /= float(subnum[i])
-                    subresult = subresult*sub
-            result += subresult  
-
-    et_result['text'] = et_result['text'] + f'\n={result}'
-
-def btn_restartclick(event):
-    et_result['text'] = '0'
-
-def btn_delclick(event):
-    if len(et_result['text'])==1:
-        et_result['text'] = '0'
-    elif et_result['text'] != '0':
-        et_result['text'] = et_result['text'][:-1]
-
-        
 #menu
 menubar = Menu(master=window)
 
@@ -135,7 +78,6 @@ btn_del = Button(window,text='Del',font=(textfont, fontbuttonsize))
 btn_del.grid(row=2,column=0,padx=(10,1),pady=1,sticky=NSEW)
 btn_del.bind('<Enter>', btn_enter)
 btn_del.bind('<Leave>', btn_leave)
-btn_del.bind('<Button-1>', btn_delclick)
 
 btn_CE = Button(window,text='CE',font=(textfont, fontbuttonsize))
 btn_CE.grid(row=2,column=1,padx=1,pady=1,sticky=NSEW)
@@ -146,7 +88,6 @@ btn_C = Button(window,text='C',font=(textfont, fontbuttonsize))
 btn_C.grid(row=2,column=2,padx=1,pady=1,sticky=NSEW)
 btn_C.bind('<Enter>', btn_enter)
 btn_C.bind('<Leave>', btn_leave)
-btn_C.bind('<Button-1>', btn_restartclick)
 
 btn_pl = Button(window,text='+-',font=(textfont, fontbuttonsize))
 btn_pl.grid(row=2,column=3,padx=1,pady=1,sticky=NSEW)
@@ -157,61 +98,51 @@ btn_sqr = Button(window,text='√',font=(textfont, fontbuttonsize))
 btn_sqr.grid(row=2,column=4,padx=(1,10),pady=1,sticky=NSEW)
 btn_sqr.bind('<Enter>', btn_enter)
 btn_sqr.bind('<Leave>', btn_leave)
-btn_sqr.bind('<Button-1>', btn_click)
 
 btn_7 = Button(window,text='7',font=(textfont, fontbuttonsize))
 btn_7.grid(row=3,column=0,padx=(10,1),pady=1,sticky=NSEW)
 btn_7.bind('<Enter>', btn_enter)
 btn_7.bind('<Leave>', btn_leave)
-btn_7.bind('<Button-1>', btn_click)
 
 btn_8 = Button(window,text='8',font=(textfont, fontbuttonsize))
 btn_8.grid(row=3,column=1,padx=1,pady=1,sticky=NSEW)
 btn_8.bind('<Enter>', btn_enter)
 btn_8.bind('<Leave>', btn_leave)
-btn_8.bind('<Button-1>', btn_click)
 
 btn_9 = Button(window,text='9',font=(textfont, fontbuttonsize))
 btn_9.grid(row=3,column=2,padx=1,pady=1,sticky=NSEW)
 btn_9.bind('<Enter>', btn_enter)
 btn_9.bind('<Leave>', btn_leave)
-btn_9.bind('<Button-1>', btn_click)
 
 btn_division = Button(window,text='/',font=(textfont, fontbuttonsize))
 btn_division.grid(row=3,column=3,padx=1,pady=1,sticky=NSEW)
 btn_division.bind('<Enter>', btn_enter)
 btn_division.bind('<Leave>', btn_leave)
-btn_division.bind('<Button-1>', btn_click)
 
 btn_per = Button(window,text='%',font=(textfont, fontbuttonsize))
 btn_per.grid(row=3,column=4,padx=(1,10),pady=1,sticky=NSEW)
 btn_per.bind('<Enter>', btn_enter)
 btn_per.bind('<Leave>', btn_leave)
-btn_per.bind('<Button-1>', btn_click)
 
 btn_4 = Button(window,text='4',font=(textfont, fontbuttonsize))
 btn_4.grid(row=4,column=0,padx=(10,1),pady=1,sticky=NSEW)
 btn_4.bind('<Enter>', btn_enter)
 btn_4.bind('<Leave>', btn_leave)
-btn_4.bind('<Button-1>', btn_click)
 
 btn_5 = Button(window,text='5',font=(textfont, fontbuttonsize))
 btn_5.grid(row=4,column=1,padx=1,pady=1,sticky=NSEW)
 btn_5.bind('<Enter>', btn_enter)
 btn_5.bind('<Leave>', btn_leave)
-btn_5.bind('<Button-1>', btn_click)
 
 btn_6 = Button(window,text='6',font=(textfont, fontbuttonsize))
 btn_6.grid(row=4,column=2,padx=1,pady=1,sticky=NSEW)
 btn_6.bind('<Enter>', btn_enter)
 btn_6.bind('<Leave>', btn_leave)
-btn_6.bind('<Button-1>', btn_click)
 
 btn_multiply = Button(window,text='*',font=(textfont, fontbuttonsize))
 btn_multiply.grid(row=4,column=3,padx=1,pady=1,sticky=NSEW)
 btn_multiply.bind('<Enter>', btn_enter)
 btn_multiply.bind('<Leave>', btn_leave)
-btn_multiply.bind('<Button-1>', btn_click)
 
 btn_divisionx = Button(window,text='1/x',font=(textfont, fontbuttonsize))
 btn_divisionx.grid(row=4,column=4,padx=(1,10),pady=1,sticky=NSEW)
@@ -222,49 +153,41 @@ btn_1 = Button(window,text='1',font=(textfont, fontbuttonsize))
 btn_1.grid(row=5,column=0,padx=(10,1),pady=1,sticky=NSEW)
 btn_1.bind('<Enter>', btn_enter)
 btn_1.bind('<Leave>', btn_leave)
-btn_1.bind('<Button-1>', btn_click)
 
 btn_2 = Button(window,text='2',font=(textfont, fontbuttonsize))
 btn_2.grid(row=5,column=1,padx=1,pady=1,sticky=NSEW)
 btn_2.bind('<Enter>', btn_enter)
 btn_2.bind('<Leave>', btn_leave)
-btn_2.bind('<Button-1>', btn_click)
 
 btn_3 = Button(window,text='3',font=(textfont, fontbuttonsize))
 btn_3.grid(row=5,column=2,padx=1,pady=1,sticky=NSEW)
 btn_3.bind('<Enter>', btn_enter)
 btn_3.bind('<Leave>', btn_leave)
-btn_3.bind('<Button-1>', btn_click)
 
 btn_minus = Button(window,text='-',font=(textfont, fontbuttonsize))
 btn_minus.grid(row=5,column=3,padx=1,pady=1,sticky=NSEW)
 btn_minus.bind('<Enter>', btn_enter)
 btn_minus.bind('<Leave>', btn_leave)
-btn_minus.bind('<Button-1>', btn_click)
 
 btn_equal = Button(window,text='=',font=(textfont, fontbuttonsize))
 btn_equal.grid(row=5,rowspan=2,column=4,padx=(1,10),pady=(1,10),sticky=NSEW)
 btn_equal.bind('<Enter>', btn_enter)
 btn_equal.bind('<Leave>', btn_leave)
-btn_equal.bind('<Button-1>', btn_equalclick)
 
 btn_0 = Button(window,text='0',font=(textfont, fontbuttonsize))
 btn_0.grid(row=6,columnspan=2,column=0,padx=(10,1),pady=(1,10),sticky=NSEW)
 btn_0.bind('<Enter>', btn_enter)
 btn_0.bind('<Leave>', btn_leave)
-btn_0.bind('<Button-1>', btn_click)
 
 btn_dot = Button(window,text='.',font=(textfont, fontbuttonsize))
 btn_dot.grid(row=6,column=2,padx=1,pady=(1,10),sticky=NSEW)
 btn_dot.bind('<Enter>', btn_enter)
 btn_dot.bind('<Leave>', btn_leave)
-btn_dot.bind('<Button-1>', btn_click)
 
 btn_plus = Button(window,text='+',font=(textfont, fontbuttonsize))
 btn_plus.grid(row=6,column=3,padx=1,pady=(1,10),sticky=NSEW)
 btn_plus.bind('<Enter>', btn_enter)
 btn_plus.bind('<Leave>', btn_leave)
-btn_plus.bind('<Button-1>', btn_click)
 
 window.config(menu=menubar)
 window.mainloop()
